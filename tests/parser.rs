cafaa68941b889343c847273975f5483e6a78186
ganmacs <ganmacs@gmail.com>
2017-01-22 17:09:21 +0900

    Add tests

---
+++ tests/parser.rs
@@ -0,0 +1,17 @@
+extern crate rlisp;
+
+use rlisp::parser::parse;
+use rlisp::Node;
+
+#[test]
+fn test_read_nil() {
+    assert_eq!(parse("()").unwrap(), Node::Nil);
+}
+
+
+#[test]
+fn test_read_int() {
+    assert_eq!(parse("1").unwrap(), Node::Int(1));
+    assert_eq!(parse("100").unwrap(), Node::Int(100));
+    assert_eq!(parse("1000").unwrap(), Node::Int(1000));
+}
