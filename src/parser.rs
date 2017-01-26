6f35972da24e819e39ef43a3726e6ca65be8ba52
ganmacs <ganmacs@gmail.com>
2017-01-26 11:55:34 +0900

    Change uncorrective names

--- src/parser.rs
+++ src/parser.rs
@@ -56,53 +56,53 @@ impl ParseError {
     }
 }
 
-fn read_quote(input: &mut Lexer) -> ParseResult {
-    let v = try!(read(input));
+fn read_quote(lexer: &mut Lexer) -> ParseResult {
+    let v = try!(read(lexer));
     Ok(node::rcell(Node::Sym("quote".to_string()), v))
 }
 
-fn read_list(input: &mut Lexer) -> ParseResult {
-    match input.peek_no_whitespace() {
+fn read_list(lexer: &mut Lexer) -> ParseResult {
+    match lexer.peek_no_whitespace() {
         None => Err(ParseError::UnmatchedParen),
         Some(')') => Ok(Node::Nil),
         _ => {
-            let car = try!(read(input));
-            let cdr = try!(read_list(input));
+            let car = try!(read(lexer));
+            let cdr = try!(read_list(lexer));
             Ok(Node::Cell(Rc::new(car), Rc::new(cdr)))
         },
     }
 }
 
-fn read_int(c: char, input: &mut Lexer) -> ParseResult {
+fn read_int(c: char, lexer: &mut Lexer) -> ParseResult {
     let radix = 10;
     let v = &mut String::new();
     v.push(c);
 
-    while let Some(n) = input.peek() {
+    while let Some(n) = lexer.peek() {
         if !n.is_digit(radix) {
             break;
         }
         v.push(n);
-        input.next();
+        lexer.next();
     }
 
     Ok(Node::Int(i32::from_str_radix(&v, radix).unwrap()))
 }
 
-fn read_number(input: &mut Lexer, c: char) ->  ParseResult {
-    read_int(c, input)
+fn read_number(lexer: &mut Lexer, c: char) ->  ParseResult {
+    read_int(c, lexer)
 }
 
-fn read_symbol(input: &mut Lexer, c: char) ->  ParseResult {
+fn read_symbol(lexer: &mut Lexer, c: char) ->  ParseResult {
     let v = &mut String::new();
     v.push(c);
 
-    while let Some(c) = input.peek() {
+    while let Some(c) = lexer.peek() {
         if !is_ident(c) {
             break;
         }
         v.push(c);
-        input.next();
+        lexer.next();
     }
 
     Ok(Node::Sym(v.to_owned()))
@@ -116,15 +116,15 @@ fn is_ident(c: char) -> bool {
     }
 }
 
-fn read(input: &mut Lexer) ->  ParseResult {
-    match input.next_no_whitespace() {
+fn read(lexer: &mut Lexer) ->  ParseResult {
+    match lexer.next_no_whitespace() {
         None => Ok(Node::Nil),
         Some(c) =>
             match c {
-                '(' => read_list(input),
-                '\'' => read_quote(input),
-                '0'...'9' => read_number(input, c),
-                _ => read_symbol(input, c)
+                '(' => read_list(lexer),
+                '\'' => read_quote(lexer),
+                '0'...'9' => read_number(lexer, c),
+                _ => read_symbol(lexer, c)
             }
     }
 }
